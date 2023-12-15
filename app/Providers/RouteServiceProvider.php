23fd5662f6991a95930cd42aa4bd5f8f7a62231d
aandriano <contact@aandriano.fr>
2023-12-15 08:42:41 +0100

    Livewire fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -35,6 +36,11 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+        });
+
+        Livewire::setUpdateRoute(function ($handle) {
+            $url =  'appname/public/livewire/update';
+            return Route::post($url, $handle);
         });
     }
 }
