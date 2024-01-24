aa9b8051c63e7779412d409ceb5fe28671ede270
aandriano <contact@aandriano.fr>
2024-01-24 21:22:30 +0100

    fix nginx

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxie = '*';
 
     /**
      * The headers that should be used to detect proxies.
